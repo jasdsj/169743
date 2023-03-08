f5090c3c28fd7a6be7aef766134bd03261a8a495
jasds2002@gmail.com <jasds2002@gmail.com>
2023-03-08 15:45:41 +0100

    yos

--- lab1.py
+++ lab1.py
@@ -17,3 +17,36 @@ i = b**1/2
 j = pow(b, 1/2)
 print(i)
 print(j)
+
+print(b)
+b += 2
+print(b)
+
+listy = ['a', 4 ,5, 6, [1, 2, 3], 5.6]
+print(listy)
+listy.append(4)
+print(listy)
+print(listy[5])
+
+lista = [1, 4, 6, 2, 5, 7]
+print(lista)
+
+#dodać element o danej pozycji
+lista.insert(2, 9)
+print(lista)
+#dodać kilka elementów
+yep = (3, 8)
+lista.extend(yep)
+print(lista)
+#usunąć element o danej pozycji
+lista.pop(1)
+print(lista)
+#usunąć element z listy o danej wartości
+lista.remove(1);
+print(lista)
+#odwrócic elementy listy
+lista.reverse()
+print(lista)
+#zrobić sortwonie
+lista.sort()
+print(lista)
